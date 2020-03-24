a08baaa11db27e1fcb06b6165e7d3627f172c692
artemsukhov <gow55511@yandex.ru>
2020-03-24 22:56:36 +0700

    new task

--- dom/temperature/celsia.js
+++ dom/temperature/celsia.js
@@ -1,9 +1,9 @@
-function getDegree(id) {
-    return parseInt(document.getElementById(id).value);
-}
-
 function getButton(className) {
     return document.getElementsByClassName(className).item(0);
+}
+
+function getDegree(id) {
+    return parseInt(document.getElementById(id).value);
 }
 
 function setDegree(id, degree) {
@@ -17,14 +17,14 @@ function setBorderColorOutputValue(id, color) {
 function handleExceptionNaN(id) {
     setBorderColorOutputValue(id, "red");
     setDegree(id, "Неверный параметр");
-    isExceptionNan = true;
+    isExceptionNaN = true;
 }
 
 function isFarengeit() {
     return document.getElementById('temperature-output-measure').value === "far"
 }
 
-function iskelvin() {
+function isKelvin() {
     return document.getElementById('temperature-output-measure').value === "kel"
 }
 
@@ -32,35 +32,41 @@ function calculateCelsiaToFarengeit(celsia) {
     return celsia * (9 / 5) + 32;
 }
 
-function calculateCelsiaToKelvun(celsia) {
+function calculateCelsiaToKelvin(celsia) {
     return celsia + 273.15;
 }
 
-var isExceptionNan = false;
+function convertEvent() {
+    var celsiaDegree = getDegree("temperature-input-value");
+
+    if (isNaN(celsiaDegree)) {
+        return handleExceptionNaN("temperature-output-value");
+    }
+
+    if (isExceptionNaN) {
+        setBorderColorOutputValue("temperature-output-value", "gray");
+        isExceptionNaN = false;
+    }
+
+    if (isFarengeit()) {
+        return setDegree("temperature-output-value", calculateCelsiaToFarengeit(celsiaDegree));
+    }
+
+    if (isKelvin()) {
+        return setDegree("temperature-output-value", calculateCelsiaToKelvin(celsiaDegree));
+    }
+}
+
+var isExceptionNaN = false;
 
 function main() {
     getButton("button-convert")
-        .addEventListener("click", function () {
-            var celsiaDegree = getDegree("temperature-input-value");
+        .addEventListener("click", () => convertEvent());
 
-            if (isNaN(celsiaDegree)) {
-                handleExceptionNaN("temperature-output-value");
-                return;
-            }
-
-            if (isExceptionNan) {
-                setBorderColorOutputValue("temperature-output-value", "gray");
-                isExceptionNan = false;
-            }
-
-            if (isFarengeit()) {
-                setDegree("temperature-output-value", calculateCelsiaToFarengeit(celsiaDegree));
-                return
-            }
-
-            if (iskelvin()) {
-                setDegree("temperature-output-value", calculateCelsiaToKelvun(celsiaDegree))
-                return;
+    document
+        .addEventListener("keypress", function (e) {
+            if (e.key === 'Enter') {
+                convertEvent();
             }
         });
 }
